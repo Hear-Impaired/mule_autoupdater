21776fd2656a9e5c54928a71bca19f840c5d43b9
hear-impaired <polarishc@gmail.com>
2021-06-08 11:57:19 +0900

    html parser init

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("Hello python!!")
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("http://www.naver.com")  
+
+bsObject = BeautifulSoup(html, "html.parser") 
+
+
+print(bsObject.head.title)
